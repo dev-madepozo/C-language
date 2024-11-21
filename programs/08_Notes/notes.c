3cef4929b3a4e0a3742b480b6fe9471b1acbede2
Manuel Del Pozo <madepozo.dev@gmail.com>
2024-11-21 01:18:47 -0500

    Notes: add logic to create database and the notes table

--- programs/08_Notes/notes.c
+++ programs/08_Notes/notes.c
@@ -14,7 +14,54 @@ typedef struct
   char updated_at[20];
 } Note;
 
+int open_db(sqlite3 **db, char *db_name)
+{
+  int rc = sqlite3_open(db_name, db);
+  if (rc != SQLITE_OK)
+  {
+    fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(*db));
+    return rc;
+  }
+
+  return rc;
+}
+
+int create_table(sqlite3 *db)
+{
+  const char *sql = "CREATE TABLE IF NOT EXISTS notes ("
+                    "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    "text TEXT NOT NULL CHECK (length(text) <= 140),"
+                    "created_at TEXT NOT NULL,"
+                    "updated_at TEXT NOT NULL);";
+  char *err_msg = 0;
+  int rc = sqlite3_exec(db, sql, 0, 0, &err_msg);
+
+  if (rc != SQLITE_OK)
+  {
+    fprintf(stderr, "SQL error: %s\n", err_msg);
+    sqlite3_free(err_msg);
+    return rc;
+  }
+
+  return 0;
+}
+
 int main(int argc, char *argv[])
 {
+  sqlite3 *db;
+  int rc = open_db(&db, "notes.db");
+
+  if (rc != SQLITE_OK)
+  {
+    return 1;
+  }
+
+  rc = create_table(db);
+
+  if (rc != SQLITE_OK)
+  {
+    return 1;
+  }
+
   return 0;
 }
